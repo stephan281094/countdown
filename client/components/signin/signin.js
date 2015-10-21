06a397fafd865feb193cbcda30fddc54fa6700b9
stephan281094 <stephan281094@gmail.com>
2015-10-21 08:56:03 +0200

    Convert tabs to spaces

--- client/components/signin/signin.js
+++ client/components/signin/signin.js
@@ -1,24 +1,24 @@
 Template.signin.events({
-	'submit .signin': function(event, template) {
-		event.preventDefault();
+  'submit .signin': function(event, template) {
+    event.preventDefault();
 
-		let input    = event.target;
-		let username = input.username.value;
-		let password = input.password.value;
+    let input    = event.target;
+    let username = input.username.value;
+    let password = input.password.value;
 
-		// If user doesn't exist yet
-		if (Match.test(Meteor.users.findOne({username: username}), undefined)) {
-			Accounts.createUser({
-				username: username,
-				password: password
-			}, (error) => {
-				console.log(error);
-			});
-		}
+    // If user doesn't exist yet
+    if (Match.test(Meteor.users.findOne({username: username}), undefined)) {
+      Accounts.createUser({
+        username: username,
+        password: password
+      }, (error) => {
+        console.log(error);
+      });
+    }
 
-		Meteor.loginWithPassword(username, password, (error) => {
-			if (!error) console.log('User successfully logged in');
-			FlowRouter.go('/');
-		});
-	}
+    Meteor.loginWithPassword(username, password, (error) => {
+      if (!error) console.log('User successfully logged in');
+      FlowRouter.go('/');
+    });
+  }
 });
